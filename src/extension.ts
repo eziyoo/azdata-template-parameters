9bf0b17fca511b8bdf8727fd45a4f4c342a4045e
Angelo Pennisi <n999kz@gmail.com>
2022-02-07 16:20:46 +0100

    Added template validation

--- src/extension.ts
+++ src/extension.ts
@@ -6,15 +6,17 @@ export function activate(context: vscode.ExtensionContext) {
         let text = activeEditor.document.getText();
         let templates = text.match(/(?<=<).*?(?=>)/g);
         templates = [...new Set(templates)];
-
+        templates = templates.filter((template) => template.split(",").length === 3);
+        
+        if (templates.length === 0) {
+            vscode.window.showErrorMessage("No valid templates found.\nValid format <NAME,TYPE,VALUE>");
+        }
         for (let template of templates) {
             let templateData = template.split(",");
-            if (templateData.length !== 3) {
-                continue;
-            }
             let templateName = templateData[0];
             let templateType = templateData[1];
             let templateValue = templateData[2];
+
             let options: vscode.InputBoxOptions = {
                 prompt: `${templateName} (Type: ${templateType.length === 0 ? "undefined": templateType})`,
                 value: templateValue
